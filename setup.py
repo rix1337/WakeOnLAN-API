db106fade6a7a994e70348500bcc6dbc900338d9
rix1337 <>
2021-03-10 11:08:42 +0100

    v.0.1.3 - Fix Readme again

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="wol_api",
-    version="0.1.2",
+    version="0.1.3",
     author="rix1337",
     author_email="",
     description="A simple wake on LAN interface that accepts post commands",
