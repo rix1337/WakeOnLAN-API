7715a5c5c121af93efdfa873fc4fb05d6307a309
rix <rix@rix>
2024-05-19 21:06:54 +0200

    v.1.0.2 - Windows fix

--- WakeOnLAN-API.py
+++ WakeOnLAN-API.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
+# WakeOnLAN-API
+# Project by https://github.com/rix1337
+
+import multiprocessing
+
 from wol_api import run
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     run.main()
--- wol_api/providers/version.py
+++ wol_api/providers/version.py
@@ -6,7 +6,7 @@ import re
 
 
 def get_version():
-    return "1.0.1"
+    return "1.0.2"
 
 
 def create_version_file():
